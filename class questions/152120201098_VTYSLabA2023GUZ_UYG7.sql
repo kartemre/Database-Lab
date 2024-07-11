b8b6b561cefc6f1093bf1a23e4c2ba6b9a6d3323
Emre Kart <76533719+kartemre@users.noreply.github.com>
2024-07-11 10:15:43 +0300

    Add files via upload

---
+++ class questions/152120201098_VTYSLabA2023GUZ_UYG7.sql
@@ -0,0 +1,103 @@
+﻿use BikeStores
+
+
+--Soru 1: Bir müşterinin tüm siparişlerini ve siparişlerin detaylarını listeleyen bir saklı yordamı yazınız.
+--Yordam, müşteri kimliğini parametre olarak almalıdır. Exec komutu ile yordamı çağırınız. (15p)
+
+CREATE PROCEDURE CustomerOrdersDetails
+    @CustomerID INT
+AS
+BEGIN
+    SELECT o.order_id, o.order_date, p.product_name, oi.quantity, oi.list_price
+    FROM sales.orders o
+    INNER JOIN sales.order_items oi ON o.order_id = oi.order_id
+    INNER JOIN production.products p ON oi.product_id = p.product_id
+    WHERE o.customer_id = @CustomerID;
+END;
+
+EXEC CustomerOrdersDetails @CustomerID = 1;
+
+
+--Soru 2: Oluşturulan saklı yordamı güncelleyerek; müşteri adı X olan kişinin en yüksek 
+--siparişini getirmesini sağlayan saklı yordamı yazınız. Exec komutu ile yordamı çağırınız. (15p)
+
+CREATE PROCEDURE HighestOrderForCustomer
+    @CustomerName NVARCHAR(100)
+AS
+BEGIN
+    SELECT TOP 1 o.order_id, o.order_date, p.product_name, oi.quantity, oi.list_price
+    FROM sales.orders o
+    INNER JOIN sales.order_items oi ON o.order_id = oi.order_id
+    INNER JOIN production.products p ON oi.product_id = p.product_id
+    INNER JOIN sales.customers c ON o.customer_id = c.customer_id
+    WHERE c.first_name = @CustomerName
+    ORDER BY oi.list_price DESC;
+END;
+
+EXEC HighestOrderForCustomer @CustomerName = "debra";
+
+
+--Soru 3: Belirli bir müşteriye ait tüm siparişlerin mağazalara göre toplam değerini hesaplayan bir saklı 
+--yordam yazın. Yordam, müşteri kimliğini parametre olarak almalıdır. Exec komutu ile yordamı çağırınız. (20p)
+
+CREATE PROCEDURE TotalOrderValueByStore
+    @CustomerID INT
+AS
+BEGIN
+    SELECT s.store_id, SUM(oi.quantity * oi.list_price) AS TotalValue
+    FROM sales.orders o
+    INNER JOIN sales.order_items oi ON o.order_id = oi.order_id
+    INNER JOIN sales.stores s ON o.store_id = s.store_id
+    WHERE o.customer_id = @CustomerID
+    GROUP BY s.store_id;
+END;
+
+EXEC TotalOrderValueByStore @CustomerID = 2;
+
+
+--Soru 4: Ürünler (products) tablosundaki en yüksek ve en düşük fiyatlı ürünlerin detaylarını 
+--(ürün adı, marka, kategori, fiyat) gösteren bir saklı yordam yazınız. Bu yordam, markalar 
+--ve kategorilerle karmaşık sorguları içermelidir. Exec komutu ile yordamı çağırınız. (20p)
+
+CREATE PROCEDURE MaxMinProductDetails
+AS
+BEGIN
+    SELECT 'Max' AS Type, p.product_name, b.brand_name, c.category_name, MAX(p.list_price) AS Price
+    FROM production.products p
+    INNER JOIN production.brands b ON p.brand_id = b.brand_id
+    INNER JOIN production.categories c ON p.category_id = c.category_id
+    WHERE p.list_price = (SELECT MAX(list_price) FROM production.products)
+    GROUP BY p.product_name, b.brand_name, c.category_name
+
+    UNION ALL
+
+    SELECT 'Min' AS Type, p.product_name, b.brand_name, c.category_name, MIN(p.list_price) AS Price
+    FROM production.products p
+    INNER JOIN production.brands b ON p.brand_id = b.brand_id
+    INNER JOIN production.categories c ON p.category_id = c.category_id
+    WHERE p.list_price = (SELECT MIN(list_price) FROM production.products)
+    GROUP BY p.product_name, b.brand_name, c.category_name;
+END;
+
+EXEC MaxMinProductDetails;
+
+
+--Soru 5: Belirli bir kategoriye ait ürünlerin sayısını ve bu ürünlerin ortalama fiyatını hesaplayan
+--bir saklı yordam yazınız. Yordam, kategori ID'sini parametre olarak almalıdır. Exec komutu ile yordamı çağırınız. (20p)
+
+CREATE PROCEDURE CategoryProductStats
+    @CategoryID INT
+AS
+BEGIN
+    SELECT COUNT(product_id) AS ProductCount, AVG(list_price) AS AvgPrice
+    FROM production.products
+    WHERE category_id = @CategoryID;
+END;
+
+EXEC CategoryProductStats @CategoryID = 3;
+
+
+--Soru 6: Oluşturulan son saklı yordamı siliniz. (10p)
+
+DROP PROCEDURE CategoryProductStats;
+
---
+++ class questions/152120201098_VTYSLabA2023GUZ_UYG8..sql
@@ -0,0 +1,86 @@
+﻿-- Soru 1: Bir müşteri silindiğinde bu müşteriye ait tüm siparişlerin otomatik olarak silinmesini
+-- sağlayan tetikleyici kodunu yazınız (20p). İlgili tetikleyici kodunu test eden SQL sorgularını
+-- yazınız.
+
+CREATE TRIGGER trg1
+ON sales.customers
+AFTER DELETE
+AS
+BEGIN
+    DELETE FROM sales.orders
+    WHERE customer_id IN (SELECT customer_id FROM DELETED)
+END
+
+--Code to test the trigger
+DELETE FROM sales.customers
+WHERE customer_id = 1
+
+-- Soru 2: Bir sipariş kalemi eklenirken ilgili ürünün stok miktarının otomatik olarak azaltılmasını
+-- sağlayarak sipariş işlemi gerçekleştiğinde stok seviyelerini gerçek zamanlı olarak güncelleyen
+-- tetikleyici kodunu yazınız (20p). İlgili tetikleyici kodunu test eden SQL sorgularını yazınız.
+
+CREATE TRIGGER trg21
+ON sales.order_items
+AFTER INSERT
+AS
+BEGIN
+    UPDATE production.stocks
+    SET quantity = quantity - (SELECT quantity FROM INSERTED)
+    WHERE product_id IN (SELECT product_id FROM INSERTED)
+END
+
+--Code to test the trigger 
+INSERT INTO sales.order_items (order_id, item_id, product_id, quantity, list_price, discount)
+
+
+-- Soru 3: Bir ürün silindiğinde bu ürüne ait stok bilgilerinin de otomatik olarak silinmesini
+-- sağlayan tetikleyici kodunu yazınız (20p). İlgili tetikleyici kodunu test eden SQL sorgularını
+-- yazınız.
+
+CREATE TRIGGER trg3
+ON production.products
+AFTER DELETE
+AS
+BEGIN
+    DELETE FROM production.stocks
+    WHERE product_id IN (SELECT product_id FROM DELETED)
+END
+
+--Code to test the trigger
+DELETE FROM production.products
+WHERE product_id = 1
+
+-- Soru 4: Yeni bir ürün eklediğinizde tüm mağazalarda stok kaydı oluşturan tetikleyici kodunu
+-- yazınız (20p). İlgili tetikleyici kodunu test eden SQL sorgularını yazınız.
+
+CREATE TRIGGER trg4
+ON production.products
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO production.stocks (store_id, product_id, quantity)
+    SELECT store_id, product_id, 0
+    FROM sales.stores, inserted
+END
+
+
+--Code to test the trigger
+INSERT INTO production.products (product_name, brand_id, category_id, model_year, list_price)
+
+-- Soru 5: Bir kategori silindiğinde, silinen kategoriye ait ürünlerin kategori bilgilerini NULL
+-- olarak güncelleyen tetikleyici kodunu yazınız (20p). İlgili tetikleyici kodunu test eden SQL
+-- sorgularını yazınız.
+
+CREATE TRIGGER trg51
+ON production.categories
+AFTER DELETE
+AS
+BEGIN
+    UPDATE production.products
+    SET category_id = NULL
+    WHERE category_id IN (SELECT category_id FROM DELETED)
+END
+
+--Code to test the trigger
+DELETE FROM production.categories
+WHERE category_id = 1
